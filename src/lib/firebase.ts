d88aa615296f8d5b13fc811fe223255ff2f4a6ce
olasunkanmi <119403527+ColourfulRhythm@users.noreply.github.com>
2025-09-22 06:38:25 +0100

    Add actual Firebase configuration for subx-825e9 project with analytics

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,15 +2,18 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
+import { getAnalytics } from 'firebase/analytics'
 
 // Firebase configuration for subx-825e9 project
 const firebaseConfig = {
-  apiKey: "AIzaSyBXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", // You'll need to get this from your Firebase console
+  apiKey: "AIzaSyC60bWkujXkpdB_jASgZhi7rb9njUXYiSc",
   authDomain: "subx-825e9.firebaseapp.com",
+  databaseURL: "https://subx-825e9-default-rtdb.firebaseio.com",
   projectId: "subx-825e9",
-  storageBucket: "subx-825e9.appspot.com",
-  messagingSenderId: "XXXXXXXXXX", // You'll need to get this from your Firebase console
-  appId: "X:XXXXXXXXXX:web:XXXXXXXXXX" // You'll need to get this from your Firebase console
+  storageBucket: "subx-825e9.firebasestorage.app",
+  messagingSenderId: "853877174483",
+  appId: "1:853877174483:web:9001636a7cd1e9160ca426",
+  measurementId: "G-FNQZQRHBVL"
 }
 
 // Initialize Firebase
@@ -20,5 +23,6 @@ const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
 export const storage = getStorage(app)
+export const analytics = getAnalytics(app)
 
 export default app
